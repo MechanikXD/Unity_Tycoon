3d0cbf23e43cec1da488134047693069dca8ef64
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-09-15 16:38:18 +0300

    Area expansion.

---
+++ Assets/Scripts/Core/AreaManager/BuildingArea.cs
@@ -0,0 +1,110 @@
+﻿using Player.Interactable;
+using UnityEngine;
+
+namespace Core.AreaManager
+{
+    public class BuildingArea : MonoBehaviour, ISceneInteractable
+    {
+        [SerializeField] private BuildingArea _prefab;
+        [SerializeField] private Vector2 _areaSize;
+        [SerializeField] private bool _isStartingArea;
+        [SerializeField] private int _originalCost;
+        [SerializeField] private float _distanceMultiplayer;
+        [SerializeField] private GameObject _mistObject;
+
+        private int _cost;
+        private bool _isAvailable;
+
+        public Vector2Int ChunkCoordinate { get; private set; }
+
+        public void Start()
+        {
+            AreaManager.Instance.AddArea(this);
+            if (_isStartingArea || _isAvailable)
+            {
+                _isAvailable = true;
+                UnlockArea();
+            }
+        }
+        
+        public void PrimaryAction()
+        {
+            // TODO: Check for cost
+            UnlockArea();
+        }
+        
+        private void SetValues(Vector2Int newCoordinates, bool isAvailable=false)
+        {
+            _isStartingArea = false;
+            ChunkCoordinate = newCoordinates;
+            var tempCost = (float)_originalCost;
+            for (var i = 0; i < Mathf.Abs(newCoordinates.x) + Mathf.Abs(newCoordinates.y) - 1; i++)
+            {
+                tempCost *= _distanceMultiplayer;
+            }
+
+            _cost = (int)tempCost;
+            
+            if (isAvailable)
+            {
+                _isAvailable = true;
+                UnlockArea();
+            }
+        }
+
+        public void UnlockArea()
+        {
+            Destroy(_mistObject);
+            CreateAdjacentAreas();
+        }
+
+        private void CreateAdjacentAreas()
+        {
+            var leftArea = ChunkCoordinate;
+            leftArea.x -= 1;
+            if (!AreaManager.Instance.HasAreaAt(leftArea))
+            {
+                var position = transform.position;
+                position.x -= _areaSize.x;
+                var instance = Instantiate(_prefab, position, Quaternion.identity);
+                instance.name = $"Building Area {leftArea}";
+                instance.SetValues(leftArea);
+            }
+            
+            var rightArea = ChunkCoordinate;
+            rightArea.x += 1;
+            if (!AreaManager.Instance.HasAreaAt(rightArea))
+            {
+                var position = transform.position;
+                position.x += _areaSize.x;
+                var instance = Instantiate(_prefab, position, Quaternion.identity);
+                instance.name = $"Building Area {rightArea}";
+                instance.SetValues(rightArea);
+            }
+            
+            var upArea = ChunkCoordinate;
+            upArea.y += 1;
+            if (!AreaManager.Instance.HasAreaAt(upArea))
+            {
+                var position = transform.position;
+                position.z += _areaSize.y;
+                var instance = Instantiate(_prefab, position, Quaternion.identity);
+                instance.name = $"Building Area {upArea}";
+                instance.SetValues(upArea);
+            }
+            
+            var downArea = ChunkCoordinate;
+            downArea.y -= 1;
+            if (!AreaManager.Instance.HasAreaAt(downArea))
+            {
+                var position = transform.position;
+                position.z -= _areaSize.y;
+                var instance = Instantiate(_prefab, position, Quaternion.identity);
+                instance.name = $"Building Area {downArea}";
+                instance.SetValues(downArea);
+            }
+        }
+        
+        public void SecondaryAction() { }
+    }
+}
