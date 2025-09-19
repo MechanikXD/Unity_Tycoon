66dcdc9180a6848815ac9677d25c62dad5c4ddf9
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-09-19 11:54:26 +0300

    Add sounds

---
+++ Assets/Scripts/Core/Audio/AudioManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Core.Behaviour.Singleton;
+using UnityEngine;
+
+namespace Core.Audio
+{
+    public class AudioManager : SingletonBase<AudioManager>
+    {
+        [SerializeField] private AudioSource _musicSource;
+
+        [SerializeField] private int _localSourcePoolCount = 31;
+        [SerializeField] private AudioSource _localSource;
+        private Queue<AudioSource> _localSourcePool;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Initialize();
+        }
+
+        public void PlaySound(AudioClip clip, Vector3 position, float reach = 20, float pitch = 1f)
+        {
+            if (_localSourcePool.Peek().isPlaying)
+            {
+                var newSource = Instantiate(_localSource, transform);
+                newSource.clip = clip;
+                newSource.transform.position = position;
+                newSource.maxDistance = reach;
+                newSource.pitch = pitch;
+
+                newSource.Play();
+                StartCoroutine(DestroyDetachedSourceAfterFinish(newSource));
+                return;
+            }
+
+            var source = _localSourcePool.Dequeue();
+
+            source.clip = clip;
+            source.transform.position = position;
+            source.maxDistance = reach;
+            source.pitch = pitch;
+
+            source.Play();
+            _localSourcePool.Enqueue(source);
+        }
+
+        private IEnumerator DestroyDetachedSourceAfterFinish(AudioSource source)
+        {
+            yield return new WaitForSeconds(source.clip.length);
+            Destroy(source.gameObject);
+        }
+
+        private void Initialize()
+        {
+            _localSourcePool = new Queue<AudioSource>();
+            for (var i = 0; i < _localSourcePoolCount; i++)
+            {
+                _localSourcePool.Enqueue(Instantiate(_localSource, transform));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Other/ButtonPlaySoundOnClick.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Core.Audio;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Other
+{
+    public class ButtonPlaySoundOnClick : MonoBehaviour
+    {
+        private Button _button;
+        [SerializeField] private AudioClip _clip;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(PLaySound);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(PLaySound);
+        }
+
+        private void PLaySound()
+        {
+            AudioManager.Instance.PlaySound(_clip, transform.position, 100f);
+        }
+    }
+}
